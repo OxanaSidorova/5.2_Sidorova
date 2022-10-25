afc9c2a500e2920d86b316ddaaa3b7ca861ce1d3
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2022-10-25 18:25:37 +0100

    added task_score.js

---
+++ task_score.js
@@ -0,0 +1,14 @@
+let introduction = 100;
+let git = 80;
+let js = 98;
+let totalScore = introduction + git +js;
+console.log (totalScore);
+let averageScore = totalScore/3;
+console.log (averageScore.toFixed(1));
+let studentName= "Oxana";
+let studentSurname = "Sidorova";
+let fullName = studentName + " " + studentSurname;
+console.log (fullName)
+console.log (`The average score of ${fullName} is ${averageScore.toFixed(1)}`)
+
+
