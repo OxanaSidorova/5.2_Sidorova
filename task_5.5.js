7c8f97083852c28a589857ba38bfc320f5999c40
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2022-10-26 10:10:45 +0100

    added description

--- task_5.5.js
+++ task_5.5.js
@@ -4,3 +4,5 @@ console.log(value2)
 let result = value1+value2/5+17
 console.log(result);
 
+/*	multiplication has 5th priority	and calculated before addition (addition has 6th prioryty). 
+if several multiplications or additions then it's calculated from left-to-right	 */
